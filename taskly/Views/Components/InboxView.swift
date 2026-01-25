e1c8c3356c33bc605b58b07ecd81ff0466373762
software78 <technicatalyst78@gmail.com>
2026-01-25 15:39:49 +0100

    Add new UI components including AddTaskBar, DailyProgressFooter, InboxView, MeshBackgroundView, TaskCard, and TaskDetailView. Refactor SidebarView for improved navigation and integrate new task management features. Enhance EmptyStateView for better user experience.

--- taskly/Views/Components/InboxView.swift
+++ taskly/Views/Components/InboxView.swift
@@ -1,5 +1,5 @@
 //
-//  HeaderView.swift
+//  InboxView.swift
 //  taskly
 //
 //  Created by Owen on 1/25/26.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct HeaderView: View {
+struct InboxView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    HeaderView()
+    InboxView()
 }
